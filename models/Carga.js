7e9d26ff8d3083493e0574737ad81762ead10de1
Gian <62362598+infoR96@users.noreply.github.com>
2022-05-20 07:36:12 -0500

    b

---
+++ models/Carga.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+
+const CargaSchema = Schema({
+//TODO cambiar model de event a carga para que tenga
+// ruc,nombre,celular,tonelaje,ticket,placa,mineral,start,end
+    ruc: {
+        type: String,
+        required: true
+    },
+    nombre: {
+        type: String,
+        required:true
+    },
+    celular:{
+        type:String,
+        requerid:true
+    },
+    ticket:{
+        type:String,
+    },
+    pesoBruto:{
+        type:Number,
+    },
+    tara:{
+        type:Number
+    },
+    pesoNeto:{
+        type:Number
+    },
+    mineral:{
+        type:String
+    },
+    start: {
+        type: Date,
+        required: true
+    },
+    end: {
+        type: Date,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true
+    }
+
+});
+
+EventoSchema.method('toJSON', function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+
+
+
+module.exports = model('Carga', CargaSchema );
+
---
+++ public/precache-manifest.c22967489e5abef74a9d809a7249e725.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed8e2833a2e8a5f3d48062cee68a4c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "991200799bc17ba71d58",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991200799bc17ba71d58",
+    "url": "/static/js/main.dd6724bf.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
